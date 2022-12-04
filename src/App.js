c5e5d488a8bfe4b01044def6867528266e297c8d
Josh Antony <joshbay84@gmail.com>
2022-12-04 19:54:38 +1100

    nosql notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import * as PropTypes from "prop-types";
+import {Component} from "react";
+
+class Lister extends Component {
+    render() {
+        return (
+            <div className="Lister">
+                {this.props.name}
+                <ul>
+                    {
+                        this.props.val.map((object, i) => <li className="Align-Left">{object}</li>)
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
+
+Lister.propTypes = {
+    val: PropTypes.arrayOf(PropTypes.string),
+    name: PropTypes.string
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="Container">
+                    <Lister name={"Javascript "}
+                            val={["closure", "this keyword", "hoisting", "callback vs promise", "prototype"]}>
+                    </Lister>
+                    <Lister name={"Java"} val={["why is string immutable"]}/>
+
+                    <Lister name={"NoSQL"} val={["volume", "velocity(too many read & write)", "variability",
+                        "memcache", "Google MapReduce", "Google BigTable", "AmazonDynamo", "Consistent hashing",
+                    "Eventual consistency", "Trades consistency for availability", "No Locking", "Allows reports to run in parallel",
+                    "CAP - applied only when network failures", "single process - C and A", "partition : choose between C and A",
+                    "ACID v BASE", "never block a write", "focus on throughput not consistency", "optimistic (failed service will eventually catch up)",
+                    "reports may be inconsistent","keep things simple and avoid locks"]}/>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
